504de8ee41b085403e276c49583c06575b21097f
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-15 12:31:34 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,8 +12,8 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
-  def genre_id=(name)
-    self.genre = Genre.find_by(name: name)
+  def genre_id=(id)
+    self.genre = Genre.find(id)
   end
 
 
